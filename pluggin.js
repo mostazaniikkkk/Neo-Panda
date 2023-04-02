c28243431c695caaf751f31e417ea4c9913b8b01
sergio valle hidalgo <ser.valle@alumnos.duoc.cl>
2023-04-01 23:41:01 -0300

    Initial commit

---
+++ pluggin.js
@@ -0,0 +1,20 @@
+neopanda = "NeoPanda: "
+console.log(neopanda + "inicializado correctamente");
+
+//Inicializar elementos
+const nuevoGif = "https://media.discordapp.net/attachments/761020322961948673/1091822289873535108/2023-03-31-20-18-58.gif?width=499&height=499"; // Definir el nuevo valor del atributo src
+const viejaFoto = "https://yt3.googleusercontent.com/ytc/AL5GRJUFL_q0g8ndDnQ3wogewfEYBx0YSvlcBo6ZzmdE=s176-c-k-c0x00ffffff-no-rj"
+
+const css = document.createElement("link");
+css.rel = "stylesheet";
+css.type = "text/css";
+css.href = "file:///E:/Descargas/Youtube/external/yt-text.css";
+document.getElementsByTagName("head")[0].appendChild(css);
+
+var div = document.getElementById("links-holder"); // obtener el elemento div por su id
+console.log(neopanda + div);
+//div.remove(); // eliminar el elemento div
+
+window.onload = function() {
+    // Crear el div nuevo
+  };
