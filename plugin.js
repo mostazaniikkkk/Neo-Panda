6368b88be6d45d754e0055671c0073e8a4356784
sergio valle hidalgo <ser.valle@alumnos.duoc.cl>
2023-04-02 10:01:47 -0400

    refactorizacion del codigo

--- plugin.js
+++ plugin.js
@@ -2,16 +2,27 @@ neopanda = "NeoPanda: "
 console.log(neopanda + "inicializado correctamente");
 
 //Inicializar elementos
-const nuevoGif = "https://media.discordapp.net/attachments/761020322961948673/1091822289873535108/2023-03-31-20-18-58.gif?width=499&height=499"; // Definir el nuevo valor del atributo src
-const viejaFoto = "https://yt3.googleusercontent.com/ytc/AL5GRJUFL_q0g8ndDnQ3wogewfEYBx0YSvlcBo6ZzmdE=s176-c-k-c0x00ffffff-no-rj"
+function isDebug(){
+    var debug = false;
+    const css = document.createElement("link");
+    css.rel = "stylesheet";
+    css.type = "text/css";
+    var div = document.getElementById("links-holder");
+    if(div == null){
+        css.href = "file:///E:/Descargas/Youtube/external/yt-text.css";
+        debug = true;
+    } else {
+        css.href = "https://cdn.statically.io/gh/mostazaniikkkk/Neo-Panda/main/yt-text.css";
+        var div = document.getElementById("links-holder"); // obtener el elemento div por su id
+        console.log(neopanda + div);
+        div.remove(); // eliminar el elemento div
+        console.log("atata")
+    }
+    document.getElementsByTagName("head")[0].appendChild(css);
+    return debug;
+}
 
-const css = document.createElement("link");
-css.rel = "stylesheet";
-css.type = "text/css";
-css.href = "https://cdn.statically.io/gh/mostazaniikkkk/Neo-Panda/main/yt-text.css";
-document.getElementsByTagName("head")[0].appendChild(css);
-
-var div = document.getElementById("links-holder"); // obtener el elemento div por su id
-console.log(neopanda + div);
-div.remove(); // eliminar el elemento div
-console.log("atata")
+//Definicion debug
+console.log(neopanda + "Iniciando test")
+const debug = isDebug();
+console.log(neopanda + "El modo debug se encuentra en " +debug);
